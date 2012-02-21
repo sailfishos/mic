7c6b7a35185ddc43f72166b666b662e3b4c1ee41
David Greaves <david@dgreaves.com>
2012-02-21 21:32:48 +0000

    Add 486 to permitted arch list

--- mic/utils/rpmmisc.py
+++ mic/utils/rpmmisc.py
@@ -350,6 +350,7 @@ archPolicies = {
     "x86_64":       "x86_64:i686:i586:i486:i386",
     "i686":         "i686:i586:i486:i386",
     "i586":         "i586:i486:i386",
+    "i486":         "i486:i386",
     "ia64":         "ia64:i686:i586:i486:i386",
     "armv7tnhl":    "armv7tnhl:armv7thl:armv7nhl:armv7hl",
     "armv7thl":     "armv7thl:armv7hl",
