68ab3a5d378eb830aea1c6dfb2bbec2d7877d75c
Juho Hämäläinen <juho.hamalainen@jolla.com>
2020-05-06 09:55:52 +0300

    mic/imager/baseimager: rpm transaction results are bytes objects.
    Need to use bytes for matching.
    
    [mic] Fix exception with mixing bytes and strings. JB#49612
    
    Signed-off-by: Juho Hämäläinen <juho.hamalainen@jolla.com>

--- mic/imager/baseimager.py
+++ mic/imager/baseimager.py
@@ -480,7 +480,7 @@ class BaseImageCreator(object):
         def get_version(header):
             version = None
             for f in header['filenames']:
-                if f.startswith('/boot/vmlinuz-'):
+                if f.startswith(b'/boot/vmlinuz-'):
                     version = f[14:]
             return version
 
